543b06bd289e46f751b852c21c2f37e8de0f3705
themonster3004 <nqd5758@gmail.com>
2023-08-24 12:42:36 +0700

    🅰️ Lesson4
    - 🅰️ CameraFollow Script attach to the Main Camera to follow the Car object (using SmoothDamp method to control position of Camera)
    - 🅰️ Enum class: DriveMode
    - Player can toggle between Manual and Automatic drive move by pressing the M key

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Tooltip("Object that the camera following")]
+    public Transform TargetObject;
+
+    [Tooltip("Camera")]
+    public Transform CamTransform;
+
+    [Tooltip("Offset between camera and following object ")]
+    public Vector3 offSet;
+
+    [Range(0.0f, 1.0f)]
+    public float SmoothTime = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        offSet = CamTransform.position - TargetObject.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPos = TargetObject.position + offSet;
+
+        CamTransform.position = Vector3.SmoothDamp(CamTransform.position, newPos, ref velocity, SmoothTime);
+        transform.LookAt(TargetObject);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -16,7 +17,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Transform bottomTransfrom;
 
 
+    [Header("Engine")]
     public float acceleration = 500f;
+    [Range(0, 2000)]
+    public float speed = 200f;
     public float breakingForce = 300f;
     public float maxTurnAngle = 15f;
 
@@ -25,41 +29,61 @@ public class PlayerController : MonoBehaviour
     private float currentBreakForce = 0f;
     private float currentTurnAngle = 0f;
 
+    private bool flag = false;
+    private DriveMode driveMode;
+
+    private void Start()
+    {
+        driveMode = DriveMode.MANUAL;
+    }
+
+    private void Update()
+    {
+        // Manual Mode
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (flag)
+            {
+                driveMode = DriveMode.MANUAL;
+                Debug.Log("Manual mode: ON!");
+            }
+            else
+            {
+                driveMode = DriveMode.AUTOMATIC;
+                Debug.Log("Automatic mode: ON!");
+            }
+            flag = !flag;
+        }
+    }
+
     private void FixedUpdate()
     {
-
-        // Get forward/reverse acceleration from the vertical axis
-        currentAcceleration = acceleration * Input.GetAxis("Vertical");
-        // Debug.DrawRay to visualize the acceleration force
         
-
-        // Giving currentBreakingForce value when from the space
-        if (Input.GetKey(KeyCode.Space))
+        if(driveMode == DriveMode.MANUAL)
         {
-            currentBreakForce = breakingForce;
+            // Get forward/reverse acceleration from the vertical axis
+            currentAcceleration = acceleration * Input.GetAxis("Vertical");
+            AddAcceleration();
+
+            // Giving currentBreakingForce value when from the space
+            if (Input.GetKey(KeyCode.Space))
+            {
+                currentBreakForce = breakingForce;
+            }
+            else currentBreakForce = 0f;
+
+            // Apply acceleration to stearing wheels 
+            currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+            AddSteering();
+        } else if (driveMode == DriveMode.AUTOMATIC)
+        {
+            currentAcceleration = acceleration * Time.deltaTime * speed;
+            Debug.Log("curAcc = " + currentAcceleration);
+            AddAcceleration();
         }
-        else currentBreakForce = 0f;
-
-        // Apply acceleration to front wheels
-        backLeft.motorTorque = currentAcceleration;
-        backRight.motorTorque = currentAcceleration;
-
-        frontLeft.brakeTorque = currentBreakForce;
-        frontRight.brakeTorque = currentBreakForce;
-        backLeft.brakeTorque= currentBreakForce;
-        backRight.brakeTorque = currentBreakForce;
-
-
-        // Apply acceleration to stearing wheels 
-        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
-
-        frontLeft.steerAngle = currentTurnAngle;
-        frontRight.steerAngle = currentTurnAngle;
-        top.steerAngle = currentTurnAngle;
-
+        
         // Update wheel meshes
-        UpdateWheel(top, topTransfrom);
-        UpdateWheel(bottom, bottomTransfrom);
+        UpdateMesh();
     }
 
     void UpdateWheel(WheelCollider collider, Transform transform)
@@ -73,4 +97,35 @@ public class PlayerController : MonoBehaviour
         transform.position = pos;
         transform.rotation = rot;
     }
+
+
+    void AddAcceleration()
+    {
+        // Apply acceleration to front wheels
+        backLeft.motorTorque = currentAcceleration;
+        backRight.motorTorque = currentAcceleration;
+
+        frontLeft.brakeTorque = currentBreakForce;
+        frontRight.brakeTorque = currentBreakForce;
+        backLeft.brakeTorque = currentBreakForce;
+        backRight.brakeTorque = currentBreakForce;
+    }
+
+    void AddSteering()
+    {
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+        top.steerAngle = currentTurnAngle;
+    }
+    void UpdateMesh()
+    {
+        UpdateWheel(top, topTransfrom);
+        UpdateWheel(bottom, bottomTransfrom);
+    }
+}
+
+public enum DriveMode
+{
+    MANUAL,
+    AUTOMATIC
 }
